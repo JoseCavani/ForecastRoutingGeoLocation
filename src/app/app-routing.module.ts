da99d08830b04f4b79c79effbe3fbe6e346c07a8
JoseCavani <ze.carlos98@hotmail.com>
2022-06-09 23:06:34 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'tempo', loadChildren: () => import('./modulos/tempo/tempo.module').then(m => m.TempoModule) }, { path: 'poluicao', loadChildren: () => import('./modulos/poluicao/poluicao.module').then(m => m.PoluicaoModule) }];
+const routes: Routes = [ { path: '', redirectTo: '/tempo/tempo', pathMatch: 'full' }, { path: 'tempo', loadChildren: () => import('./modulos/tempo/tempo.module').then(m => m.TempoModule) }, { path: 'poluicao', loadChildren: () => import('./modulos/poluicao/poluicao.module').then(m => m.PoluicaoModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
